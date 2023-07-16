a73d765c8409d4a0d1c47f5100fdc17e4e931bea
HomeWindows <yangpan0822@outlook.com>
2023-07-16 22:38:13 +0800

    @add 添加第三方库glfw

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,28 @@
+
+#define GLFW_INCLUDE_VULKAN // 定义这个宏之后，glfw3.h文件就会包含vulkan的头文件
+
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello Vulkan\n";
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
