234d5e58e52116a601842faf43dc23e5bdeb0f78
worker-Windows <yangpan0822@qq.com>
2023-07-17 11:11:54 +0800

    @edit 创建Vulkan实例

--- sources/main.cpp
+++ sources/main.cpp
@@ -13,17 +13,15 @@
  * 10.从交换链获取图像进行绘制操作，提交图像对应的指令缓冲，返回图像到交换链
  */
 
-#define GLFW_INCLUDE_VULKAN // 定义这个宏之后，glfw3.h文件就会包含vulkan的头文件
+#define GLFW_INCLUDE_VULKAN // 定义这个宏之后 glfw3.h 文件就会包含 Vulkan 的头文件
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 
 class HelloTriangleApplication
 {
-private:
-    GLFWwindow* m_window { nullptr };
-
 public:
     void Run()
     {
@@ -38,19 +36,22 @@ private:
     {
         if (GLFW_FALSE == glfwInit())
         {
-            throw std::runtime_error("GLFW init failed");
+            throw std::runtime_error("failed to init GLFW");
         }
 
+        // 禁止 glfw 创建 OpenGL 上下文
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        // 禁止窗口大小的改变
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
         m_window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
     }
 
     void InitVulkan()
     {
+        CreateInstance();
     }
 
-    void MainLoop()
+    void MainLoop() const noexcept
     {
         while (!glfwWindowShouldClose(m_window))
         {
@@ -58,11 +59,77 @@ private:
         }
     }
 
-    void Cleanup()
+    void Cleanup() noexcept
     {
+        // 程序结束前清理 Vulkan 实例
+        vkDestroyInstance(m_instance, nullptr);
+
         glfwDestroyWindow(m_window);
         glfwTerminate();
     }
+
+private:
+    void CreateInstance()
+    {
+        // 应用程序的信息，这些信息可能会作为驱动程序的优化依据，让驱动做一些特殊的优化
+        VkApplicationInfo appInfo  = {};
+        appInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pNext              = nullptr;
+        appInfo.pApplicationName   = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName        = "No Engine";
+        appInfo.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion         = VK_API_VERSION_1_0;
+
+        // Vulkan 是一个与平台无关的 API ，所以需要一个和窗口系统交互的扩展
+        // 使用 glfw 获取这个扩展
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        std::cout << "-------------------------------------------\n"
+                  << "GLFW extensions:\n";
+        for (size_t i = 0; i < glfwExtensionCount; ++i)
+        {
+            std::cout << glfwExtensions[i] << '\n';
+        }
+
+        // 指定驱动程序需要使用的全局扩展和校验层，全局是指对整个应用程序都有效，而不仅仅是某一个设备
+        VkInstanceCreateInfo createInfo = {};
+        createInfo.sType                = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo     = &appInfo;
+
+        // 指定需要的全局扩展
+        createInfo.enabledExtensionCount   = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        // 指定全局校验层
+        createInfo.enabledLayerCount = 0;
+
+        // 获取 Vulkan 支持的所有扩展
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "-------------------------------------------\n"
+                  << "All supported extensions:\n";
+        for (const auto& e : extensions)
+        {
+            std::cout << e.extensionName << '\n';
+        }
+
+        // 创建 Vulkan 实例，用来初始化 Vulkan 库
+        // 1.包含创建信息的结构体指针
+        // 2.自定义的分配器回调函数
+        // 3.指向实例句柄存储位置的指针
+        if (VK_SUCCESS != vkCreateInstance(&createInfo, nullptr, &m_instance))
+        {
+            throw std::runtime_error("failed to create instance");
+        }
+    }
+
+private:
+    GLFWwindow* m_window { nullptr };
+    VkInstance m_instance { nullptr };
 };
 
 int main()
