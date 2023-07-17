8de359abec35a92f59fb82f68a7bd1fbc75a93b5
HomeWindows <yangpan0822@outlook.com>
2023-07-17 23:26:28 +0800

    @edit 固定功能

--- sources/main.cpp
+++ sources/main.cpp
@@ -112,6 +112,7 @@ private:
         }
 
         vkDestroySwapchainKHR(m_device, m_swapChain, nullptr);
+        vkDestroyPipelineLayout(m_device, m_pipelineLayout, nullptr);
         vkDestroyDevice(m_device, nullptr);
 
         if (g_enableValidationLayers)
@@ -670,7 +671,7 @@ private:
 
     /// @brief 创建图形管线
     /// @details 在 Vulkan 中几乎不允许对图形管线进行动态设置，也就意味着每一种状态都需要提前创建一个图形管线
-    void CreateGraphicsPipeline() const noexcept
+    void CreateGraphicsPipeline()
     {
         auto vertShaderCode = ReadFile("resources/shaders/vert.spv");
         auto fragShaderCode = ReadFile("resources/shaders/frag.spv");
@@ -694,13 +695,109 @@ private:
 
         VkPipelineShaderStageCreateInfo shaderStages[] = { vertShaderStageInfo, fragShaderStageInfo };
 
+        // 顶点信息
+        VkPipelineVertexInputStateCreateInfo vertexInputInfo = {};
+        vertexInputInfo.sType                                = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+        vertexInputInfo.vertexBindingDescriptionCount        = 0;
+        vertexInputInfo.pVertexBindingDescriptions           = nullptr; // 可选的
+        vertexInputInfo.vertexAttributeDescriptionCount      = 0;
+        vertexInputInfo.pVertexAttributeDescriptions         = nullptr; // 可选的
+
+        // 拓扑信息
+        VkPipelineInputAssemblyStateCreateInfo inputAssembly = {};
+        inputAssembly.sType                                  = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        inputAssembly.topology                               = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+        inputAssembly.primitiveRestartEnable                 = VK_FALSE;
+
+        // 视口
+        VkViewport viewport = {};
+        viewport.x          = 0.f;
+        viewport.y          = 0.f;
+        viewport.width      = static_cast<float>(m_swapChainExtent.width);
+        viewport.height     = static_cast<float>(m_swapChainExtent.height);
+        viewport.minDepth   = 0.f; // 深度值范围，必须在 [0.f, 1.f]之间
+        viewport.maxDepth   = 1.f;
+
+        // 裁剪
+        VkRect2D scissor = {};
+        scissor.offset   = { 0, 0 };
+        scissor.extent   = m_swapChainExtent;
+
+        VkPipelineViewportStateCreateInfo viewportState = {};
+        viewportState.sType                             = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+        viewportState.viewportCount                     = 1;
+        viewportState.pViewports                        = &viewport;
+        viewportState.scissorCount                      = 1;
+        viewportState.pScissors                         = &scissor;
+
+        // 光栅化
+        VkPipelineRasterizationStateCreateInfo rasterizer = {};
+        rasterizer.sType                                  = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+        rasterizer.depthClampEnable                       = VK_FALSE;
+        rasterizer.rasterizerDiscardEnable                = VK_FALSE; // 设置为true会禁止一切片段输出到帧缓冲
+        rasterizer.polygonMode                            = VK_POLYGON_MODE_FILL;
+        rasterizer.lineWidth                              = 1.f;
+        rasterizer.cullMode                               = VK_CULL_MODE_BACK_BIT;   // 表面剔除类型，正面、背面、双面剔除
+        rasterizer.frontFace                              = VK_FRONT_FACE_CLOCKWISE; // 指定顺时针的顶点序是正面还是反面
+        rasterizer.depthBiasEnable                        = VK_FALSE;
+        rasterizer.depthBiasConstantFactor                = 1.f;
+        rasterizer.depthBiasClamp                         = 0.f;
+        rasterizer.depthBiasSlopeFactor                   = 0.f;
+
+        // 多重采样
+        VkPipelineMultisampleStateCreateInfo multisampling = {};
+        multisampling.sType                                = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+        multisampling.sampleShadingEnable                  = VK_FALSE;
+        multisampling.rasterizationSamples                 = VK_SAMPLE_COUNT_1_BIT;
+        multisampling.minSampleShading                     = 1.f;
+        multisampling.pSampleMask                          = nullptr;
+        multisampling.alphaToCoverageEnable                = VK_FALSE;
+        multisampling.alphaToOneEnable                     = VK_FALSE;
+
+        // 深度和模板测试
+
+        // 颜色混合，可以对指定帧缓冲单独设置，也可以设置全局颜色混合方式
+        VkPipelineColorBlendAttachmentState colorBlendAttachment {};
+        colorBlendAttachment.colorWriteMask
+            = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+        colorBlendAttachment.blendEnable = VK_FALSE;
+
+        VkPipelineColorBlendStateCreateInfo colorBlending {};
+        colorBlending.sType             = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+        colorBlending.logicOpEnable     = VK_FALSE;
+        colorBlending.logicOp           = VK_LOGIC_OP_COPY;
+        colorBlending.attachmentCount   = 1;
+        colorBlending.pAttachments      = &colorBlendAttachment;
+        colorBlending.blendConstants[0] = 0.0f;
+        colorBlending.blendConstants[1] = 0.0f;
+        colorBlending.blendConstants[2] = 0.0f;
+        colorBlending.blendConstants[3] = 0.0f;
+
+        // 动态状态，视口大小、线宽、混合常量等
+        std::vector<VkDynamicState> dynamicStates = { VK_DYNAMIC_STATE_VIEWPORT, VK_DYNAMIC_STATE_SCISSOR };
+        VkPipelineDynamicStateCreateInfo dynamicState {};
+        dynamicState.sType             = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+        dynamicState.dynamicStateCount = static_cast<uint32_t>(dynamicStates.size());
+        dynamicState.pDynamicStates    = dynamicStates.data();
+
+        // 管线布局，在着色器中使用 uniform 变量
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo {};
+        pipelineLayoutInfo.sType                  = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipelineLayoutInfo.setLayoutCount         = 0;
+        pipelineLayoutInfo.pushConstantRangeCount = 0;
+
+        if (VK_SUCCESS != vkCreatePipelineLayout(m_device, &pipelineLayoutInfo, nullptr, &m_pipelineLayout))
+        {
+            throw std::runtime_error("failed to create pipeline layout");
+        }
+
         vkDestroyShaderModule(m_device, fragShaderModule, nullptr);
         vkDestroyShaderModule(m_device, vertShaderModule, nullptr);
     }
 
     /// @brief 使用着色器字节码数组创建 VkShaderModule 对象
-    /// @param code 
-    /// @return 
+    /// @param code
+    /// @return
     VkShaderModule CreateShaderModule(const std::vector<char>& code) const
     {
         VkShaderModuleCreateInfo createInfo = {};
@@ -802,6 +899,7 @@ private:
     VkFormat m_swapChainImageFormat {};
     VkExtent2D m_swapChainExtent {};
     std::vector<VkImageView> m_swapChainImageViews;
+    VkPipelineLayout m_pipelineLayout { nullptr };
 };
 
 int main()
