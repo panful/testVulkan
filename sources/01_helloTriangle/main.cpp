6f4db7a7ded35bd09a93c7e017b546d46b38ed1f
worker-Windows <yangpan0822@qq.com>
2023-07-19 16:41:55 +0800

    @edit 修改shaders路径

--- sources/01_helloTriangle/main.cpp
+++ sources/01_helloTriangle/main.cpp
@@ -720,8 +720,8 @@ private:
     /// @details 在 Vulkan 中几乎不允许对图形管线进行动态设置，也就意味着每一种状态都需要提前创建一个图形管线
     void CreateGraphicsPipeline()
     {
-        auto vertShaderCode = ReadFile("resources/shaders/vert.spv");
-        auto fragShaderCode = ReadFile("resources/shaders/frag.spv");
+        auto vertShaderCode = ReadFile("../resources/shaders/01_vert.spv");
+        auto fragShaderCode = ReadFile("../resources/shaders/01_frag.spv");
 
         VkShaderModule vertShaderModule = CreateShaderModule(vertShaderCode);
         VkShaderModule fragShaderModule = CreateShaderModule(fragShaderCode);
